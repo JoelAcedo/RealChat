781d05db505c71b9f812d33efbb827a6f2761830
joelacedo <joelacedo@outlook.com>
2016-07-21 17:29:04 +0200

    0.5

--- server.js
+++ server.js
@@ -1,16 +1,23 @@
 'use strict'
 
 const http = require('http');
+const fs = require('fs');
 const port = process.env.PORT || 8080;
 
-const server = http.createServer(onRequest);
+const server = http.createServer();
+
+server.on('request', onRequest);
+server.on('listening', onListening);
+
+server.listen(port);
 
 function onRequest (req, res) {
-    res.end('Hola io.js');
+    let file = fs.readFileSync('public/index.html');
+    res.end(file);
 };
 
 function onListening (req, res) {
     console.log('Servidor escuchando en puerto: ' + port);
 };
 
-server.listen(port, onListening);
+
