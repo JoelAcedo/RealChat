26d2439a0e23af4325c04f8a365106f356e03b6e
joelacedo <joelacedo@outlook.com>
2016-07-21 17:18:39 +0200

    0.4

--- server.js
+++ server.js
@@ -3,10 +3,14 @@
 const http = require('http');
 const port = process.env.PORT || 8080;
 
-const server = http.createServer(function (req, res) {
-    res.end('Hola io.js');
-});
+const server = http.createServer(onRequest);
 
-server.listen(port, function () {
+function onRequest (req, res) {
+    res.end('Hola io.js');
+};
+
+function onListening (req, res) {
     console.log('Servidor escuchando en puerto: ' + port);
-});
+};
+
+server.listen(port, onListening);
